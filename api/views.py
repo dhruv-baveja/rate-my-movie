10dd74a4cb5eb11a861ea2b9bdf30088f37bdad6
Rahul Karkera <rhlk92@gmail.com>
2016-08-11 19:04:17 +0530

    Create post method to register users

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class Register(APIView):
+    """
+    Register a new user
+    """
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.isvalid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
