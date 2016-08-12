5cd794da50512afd5cd6efe4b43baebf66bed2c4
Rahul Karkera <rhlk92@gmail.com>
2016-08-12 18:41:36 +0530

    Add create method
    This method will make sure password is stored as hash
    Also extra_kwargs are added to make sure hashed passwords are not displayed

--- api/serializers.py
+++ api/serializers.py
@@ -7,4 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-        write_only = ('password',)
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, data):
+        user = User.objects.create(
+            username=data['username'],
+            email=data['email']
+        )
+        user.set_password(data['password'])
+        user.save()
+        return user
