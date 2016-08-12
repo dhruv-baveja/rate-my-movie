9dbd335841961c4b70c6231f223981399a904a96
Rahul Karkera <rhlk92@gmail.com>
2016-08-12 18:57:49 +0530

    Add obtain_auth_token to urls.py
    This provides the ability to retreive tokens by passing the username and password

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from rest_framework.authtoken import views
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-token-auth/', views.obtain_auth_token),
 )
