21499b2aebbbb27e52c39a23c155e16ea5be2cef
Rahul Karkera <rhlk92@gmail.com>
2016-08-12 11:48:30 +0530

    Test user registration
    It tests user registeration. It tests duplicate usernames.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class RegisterTest(APITestCase):
+    def register_user(self):
+        """
+        create a new user.
+        """
+        url = reverse('register')
+        data = {"username": "testuser", "email": "test@email.com",
+                "password": "testpass"}
+        return self.client.post(url, data, format='json')
+
+    def test_register_user(self):
+        response = self.register_user()
+        self.assertEqual(response.data, {"username": "testuser",
+                                         "email": "test@email.com",
+                                         "password": "testpass"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_user(self):
+        """
+        create a duplicate user.
+        """
+        self.register_user()
+        response = self.register_user()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
