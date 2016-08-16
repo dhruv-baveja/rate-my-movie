2c391bc3565b8f2aabcc522c1e95f1b245e72d98
Rahul Karkera <rhlk92@gmail.com>
2016-08-16 18:55:12 +0530

    Add RatingList to views
    This will allow users to add ratings to movie

--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import UserSerializer, MovieSerializer
-from .models import Movie
+from .serializers import UserSerializer, MovieSerializer, RatingSerializer
+from .models import Movie, Rating
 
 
 class Register(APIView):
@@ -32,8 +32,28 @@ class MovieList(APIView):
         if serializer.is_valid():
             data = serializer.data
             owner = request.user
-            movie = Movie(user=owner, title=data['title'], description=data['description'])
+            movie = Movie(user=owner, title=data['title'],
+                          description=data['description'])
             movie.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class RatingList(APIView):
+    """
+    Add rating for a movie
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, format=None):
+        serializer = RatingSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                movie = Movie.objects.get(pk=serializer.data["movie"])
+            except Movie.DoesNotExist:
+                return Response({"detail": "Movie does not exist."})
+            data = serializer.data
+            Rating(movie=movie, user=request.user, rating=data['rating']).save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
