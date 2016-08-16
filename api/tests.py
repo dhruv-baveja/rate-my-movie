7a438c13bf64fea2c843e23c20c681ba792ddc2a
Rahul Karkera <rhlk92@gmail.com>
2016-08-16 15:04:29 +0530

    Add testcase for adding a movie

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, force_authenticate
 
 
 class RegisterTest(APITestCase):
@@ -33,3 +35,30 @@ class RegisterTest(APITestCase):
         self.register_user()
         response = self.register_user()
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class MovieTest(APITestCase):
+    def register_user(self):
+        """
+        create a new user.
+        """
+        url = reverse('register')
+        data = {"username": "testuser", "email": "test@email.com",
+                "password": "testpass"}
+        return self.client.post(url, data, format='json')
+
+    def test_add_movie(self):
+        """
+        create a new movie.
+        """
+        self.register_user()
+        user = User.objects.get(username='testuser')
+        token = Token.objects.get(user=user)
+        url = reverse('movies')
+        data = {"title": "movie title", "description": "this is description"}
+        response = self.client.post(url, data, format='json',
+                                    HTTP_AUTHORIZATION='Token {}'.format(token))
+        print response
+        self.assertEqual(response.data, {"title": "movie title",
+                                         "description": "this is description"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
