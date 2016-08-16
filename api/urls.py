e8fe3e8cf1fbd5b372d3a02df803425768644d18
Rahul Karkera <rhlk92@gmail.com>
2016-08-16 15:05:31 +0530

    Add url for MovieList view

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import Register
+from .views import Register, MovieList
 
 urlpatterns = patterns(
     '',
     url(r'^register/$', Register.as_view(), name='register'),
+    url(r'^movies/$', MovieList.as_view(), name='movies'),
 )
