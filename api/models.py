4930003d8a491677471cdb8c5e7802594406c786
Rahul Karkera <rhlk92@gmail.com>
2016-08-16 15:02:07 +0530

    Add Movie table to models

--- api/models.py
+++ api/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
