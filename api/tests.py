c91a725bd8ffff995f0101440cdb9350153b9a2d
Rahul Karkera <rhlk92@gmail.com>
2016-08-17 17:28:02 +0530

    Add test cases for rating movie permissions
    Test case to test user adding a movie
    Test case to test that a user can rate a movie once
    test whether a movie exists or not
    test when creator tries to rate a movie

--- api/tests.py
+++ api/tests.py
@@ -9,7 +9,7 @@ from rest_framework.test import APITestCase
 class RegisterTest(APITestCase):
     def register_user(self):
         """
-        create a new user.
+        Create a new user.
         """
         url = reverse('register')
         data = {"username": "testuser", "email": "test@email.com",
@@ -47,9 +47,18 @@ class MovieTest(APITestCase):
                 "password": "testpass"}
         return self.client.post(url, data, format='json')
 
+    def register_another_user(self):
+        """
+        create a another user.
+        """
+        url = reverse('register')
+        data = {"username": "anothertestuser", "email": "test@email.com",
+                "password": "testpass"}
+        return self.client.post(url, data, format='json')
+
     def test_add_movie(self):
         """
-        create a new movie.
+        Create a new movie.
         """
         self.register_user()
         user = User.objects.get(username='testuser')
@@ -65,14 +74,39 @@ class MovieTest(APITestCase):
 
     def test_add_rating(self):
         """
-        add rating to a movie.
+        Add rating to a movie.
         """
         self.test_add_movie()
         user = User.objects.get(username='testuser')
         token = Token.objects.get(user=user)
+
+        # Test permission denied for user who has added the movie
         url = reverse('ratings')
+        data = {"movie": 1, "rating": 4}
+        response = self.client.post(url, data, format='json',
+                                    HTTP_AUTHORIZATION='Token {}'.format(token))
+        self.assertEqual(response.data, {'detail': 'You do not have permission to perform this action.'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Test not found error when user tries to rate a non existing movie
+        self.register_another_user()
+        data = {"movie": 2, "rating": 4}
+        user = User.objects.get(username='anothertestuser')
+        token = Token.objects.get(user=user)
+        response = self.client.post(url, data, format='json',
+                                    HTTP_AUTHORIZATION='Token {}'.format(token))
+        self.assertEqual(response.data, {'detail': 'Not found.'})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # Test whether the user can rate a movie
         data = {"movie": 1, "rating": 4}
         response = self.client.post(url, data, format='json',
                                     HTTP_AUTHORIZATION='Token {}'.format(token))
         self.assertEqual(response.data, {"movie": 1, "rating": 4})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Test permission denied  when user attempts to rate a movie twice
+        response = self.client.post(url, data, format='json',
+                                    HTTP_AUTHORIZATION='Token {}'.format(token))
+        self.assertEqual(response.data, {'detail': 'You do not have permission to perform this action.'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
